c03a57ec6a0d1a8b345e604ad8d832c56f5d2caa
Youta Obby Sidane <dyosby237@gmail.com>
2023-09-04 12:27:46 +0200

    Add preview image

--- src/App.js
+++ src/App.js
@@ -21,11 +21,31 @@ Amplify.configure({
 const App = ({ signOut, user }) => {
   const mounted = useRef(false);
   const [resources, setResources] = useState(null);
+  const [imageLink, setImageLink] = useState(null);
 
-  const sendForm = () => {
+  const sendForm = (e) => {
+    e.preventDefault();
+    if (!e.target.file?.files?.length) {
+      alert('Vous devez selectionner un fichier.')
+      return;
+    }
     console.log('loll')
   }
 
+  const toggleResetBtn = () => {
+    setImageLink(null);
+  }
+
+  const uploadFile = (e) => {
+    let file = e.target?.files;
+    let imageElement = document.getElementById('fileInput');
+    if (file && file[0] && imageElement) {
+      imageElement.src = URL.createObjectURL(file[0]);
+      setImageLink(e.target.value);
+    }
+
+    console.log('upload')
+  }
 
   const deleteImage = (id) => {
     console.log('id')
@@ -99,111 +119,32 @@ const App = ({ signOut, user }) => {
 
         <div>
           <div className="mb-4">
-            <h2 className="font-bold text-xl">
+            <h2 className="font-bold text-xl mb-5">
               Ajouter un fichier a votre stockage Cloud
             </h2>
-            <form action="" onSubmit={sendForm} encType="multipart/form-data">
-              <div className="mb-6 pt-4">
-                <label className="mb-5 block text-xl font-semibold text-[#07074D]">
-                  Upload File
-                </label>
-
-                <div className="mb-8">
-                  <input type="file" name="file" id="file" className="sr-only" />
-                  <label
-                    htmlFor="file"
-                    className="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center"
-                  >
-                    <div>
-            <span className="mb-2 block text-xl font-semibold text-[#07074D]">
-              Drop files here
-            </span>
-                      <span className="mb-2 block text-base font-medium text-[#6B7280]">
-              Or
-            </span>
-                      <span
-                        className="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]"
-                      >
-              Browse
-            </span>
-                    </div>
-                  </label>
-                </div>
-
-                <div className="mb-5 rounded-md bg-[#F5F7FB] py-4 px-8">
-                  <div className="flex items-center justify-between">
-          <span className="truncate pr-3 text-base font-medium text-[#07074D]">
-            banner-design.png
-          </span>
-                    <button className="text-[#07074D]">
-                      <svg
-                        width="10"
-                        height="10"
-                        viewBox="0 0 10 10"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M0.279337 0.279338C0.651787 -0.0931121 1.25565 -0.0931121 1.6281 0.279338L9.72066 8.3719C10.0931 8.74435 10.0931 9.34821 9.72066 9.72066C9.34821 10.0931 8.74435 10.0931 8.3719 9.72066L0.279337 1.6281C-0.0931125 1.25565 -0.0931125 0.651788 0.279337 0.279338Z"
-                          fill="currentColor"
-                        />
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M0.279337 9.72066C-0.0931125 9.34821 -0.0931125 8.74435 0.279337 8.3719L8.3719 0.279338C8.74435 -0.0931127 9.34821 -0.0931123 9.72066 0.279338C10.0931 0.651787 10.0931 1.25565 9.72066 1.6281L1.6281 9.72066C1.25565 10.0931 0.651787 10.0931 0.279337 9.72066Z"
-                          fill="currentColor"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-
-                <div className="rounded-md bg-[#F5F7FB] py-4 px-8">
-                  <div className="flex items-center justify-between">
-          <span className="truncate pr-3 text-base font-medium text-[#07074D]">
-            banner-design.png
-          </span>
-                    <button className="text-[#07074D]">
-                      <svg
-                        width="10"
-                        height="10"
-                        viewBox="0 0 10 10"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M0.279337 0.279338C0.651787 -0.0931121 1.25565 -0.0931121 1.6281 0.279338L9.72066 8.3719C10.0931 8.74435 10.0931 9.34821 9.72066 9.72066C9.34821 10.0931 8.74435 10.0931 8.3719 9.72066L0.279337 1.6281C-0.0931125 1.25565 -0.0931125 0.651788 0.279337 0.279338Z"
-                          fill="currentColor"
-                        />
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M0.279337 9.72066C-0.0931125 9.34821 -0.0931125 8.74435 0.279337 8.3719L8.3719 0.279338C8.74435 -0.0931127 9.34821 -0.0931123 9.72066 0.279338C10.0931 0.651787 10.0931 1.25565 9.72066 1.6281L1.6281 9.72066C1.25565 10.0931 0.651787 10.0931 0.279337 9.72066Z"
-                          fill="currentColor"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                  <div className="relative mt-5 h-[6px] w-full rounded-lg bg-[#E2E5EF]">
-                    <div
-                      className="absolute left-0 right-0 h-full w-[75%] rounded-lg bg-[#6A64F1]"
-                    ></div>
-                  </div>
-                </div>
+            <form onSubmit={sendForm} onReset={toggleResetBtn} encType="multipart/form-data">
+              <div className={imageLink === null ? 'hidden' : 'w-full py-3'}>
+                <img src="#" id="fileInput" alt="Preview" className="h-32" />
               </div>
+              <div className="flex py-3 items-center">
+                <div className="flex items-center">
+                  <label htmlFor="file" className="bg-indigo-600 hover:bg-indigo-800 text-white py-2 px-2 rounded-md cursor-pointer transition ease-in-out duration-300 mr-3">
+                    Choisir un fichier
+                    <input type="file" id="file" name="file" placeholder="Choisir un fichier" className="hidden" onChange={uploadFile}/>
+                  </label>
 
-              <div className="flex">
-                <label htmlFor="file" className="">
-                  Choisir un fichier
-                  <input type="file" id="file" name="file" placeholder="Choisir un fichier" className="hidden"/>
-                </label>
-                <button type="submit">
-                  Telecharger
-                </button>
+                  <button type="reset" className={imageLink === null ? 'hidden' : 'mr-5'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                  </button>
+                </div>
+
+                <div>
+                  <button type="submit" className="bg-red-400 hover:bg-red-600 text-white font-bold py-2 px-2 rounded-md transition ease-in-out duration-300">
+                    Telecharger
+                  </button>
+                </div>
               </div>
             </form>
             <div className="flex flex-col">
@@ -251,7 +192,7 @@ const App = ({ signOut, user }) => {
                           id="content" name="content" placeholder="Bonjour le monde" minLength={4}></textarea>
               </div>
 
-              <button type="submit" className="md:w-32 bg-gray-700 hover:bg-gray-900 text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300">
+              <button type="submit" className="md:w-32 bg-gray-700 hover:bg-gray-900 text-white font-bold py-3 px-6 rounded-lg mt-3 transition ease-in-out duration-300">
                 Envoyer
               </button>
             </form>
